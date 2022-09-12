a1cb665e4918d89423f073c3209a7625d11df053
Andrea Miglietta <m.andrea96@hotmail.it>
2022-09-12 17:50:40 +0200

    creazione vue

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+
+// creazione vue
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+
+        }
+
+    }
+)
